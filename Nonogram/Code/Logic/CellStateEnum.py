d9988ccc437be93fcf2294deddcbe74e445244ad
CapitanCigarro <nigarcia2023@inf.udec.cl>
2024-09-24 14:03:12 -0300

    Cell class
    working logic cell class

---
+++ Nonogram/Code/Logic/CellStateEnum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class CellStateEnum(Enum):
+    EMPTY = 0
+    PAINTED = 1
+    MARKED = 2
--- Nonogram/Code/Logic/StateEnum.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class CellState(Enum):
-    UNMARKED = 0
-    MARKED = 1
-    X = 2
