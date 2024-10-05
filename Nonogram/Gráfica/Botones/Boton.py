ee013e16c5e220f7e239f13a7befbaccd3223642
pcfuenr <panchocifuentes1234@gmail.com>
2024-10-05 19:29:36 -0300

    Añadida carpeta grafica + carpeta botones y escenas

---
+++ Nonogram/Gráfica/Botones/Boton.py
@@ -0,0 +1,6 @@
+from abc import ABC, abstractmethod
+
+class Boton(ABC):
+    @abstractmethod
+    def ActionListener():
+        pass
---
+++ Nonogram/Gráfica/Botones/BotonPintar.py
@@ -0,0 +1,5 @@
+from Botones import Boton
+
+class BotonPintar(Boton):
+    def ActionListener():
+        print("Correctamente llamado")
