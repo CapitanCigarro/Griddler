c918f090b5dd228ff98af2362014af17cb6a5233
MVergar4 <marcvergara2023@udec.cl>
2024-10-14 20:50:41 -0300

    added changeCell func and refactored class Level

--- Nonogram/Code/Logic/Level.py
+++ Nonogram/Code/Logic/Level.py
@@ -1,14 +1,16 @@
 from Grid import Grid
+from  CellStateEnum import CellStateEnum
 
 class Level:
-    __expectedGrid : Grid
     __currentGrid : Grid
     __score : int
 
-    def __init__(self, expected : Grid, current : Grid) -> None:
-        self.__expectedGrid = expected
-        self.__currentGrid = current
+    def __init__(self, expected : Grid) -> None:
+        self.__currentGrid = expected
         __score = 0
 
     def getScore(self) -> int:
         return self.__score
+
+    def changeCell(self, i : int, j : int, state : CellStateEnum) -> None:
+        self.__score = self.__currentGrid.getCell(i, j).setCurrentState(state)
