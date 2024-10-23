00908e36630b85f63f7103dac11563ec279b0e34
MVergar4 <marcvergara2023@udec.cl>
2024-10-23 15:52:31 -0300

    Fixed initial score in Level

--- Nonogram/Code/Logic/Level.py
+++ Nonogram/Code/Logic/Level.py
@@ -1,5 +1,3 @@
-from random import random, randint
-
 from Grid import Grid
 from  CellStateEnum import CellStateEnum
 
@@ -12,19 +10,24 @@ class Level:
         self.__currentGrid = expected
         self.__score = 0
         self.__remainingClues = 3
+        for i in range(self.__currentGrid.getGridRows()):
+            for j in range(self.__currentGrid.getGridColumns()):
+                if self.__currentGrid.getCell(i, j).isSolved():
+                    self.__score += 1
+
 
     def getScore(self) -> int:
         return self.__score
 
     def changeCell(self, i : int, j : int, state : CellStateEnum) -> None:
-        self.__score = self.__currentGrid.getCell(i, j).setCurrentState(state)
+        self.__score += self.__currentGrid.getCell(i, j).setCurrentState(state)
 
     def useClue(self, i : int, j : int) -> None:
         if self.__remainingClues > 0:
             clue_cell = self.__currentGrid.getCell(i, j)
             if not clue_cell.isSolved():
                 if clue_cell.getExpectedState() == CellStateEnum.PAINTED:
-                    clue_cell.setCurrentState(CellStateEnum.PAINTED)
+                    self.__score += clue_cell.setCurrentState(CellStateEnum.PAINTED)
                 else:
-                    clue_cell.setCurrentState(CellStateEnum.MARKED)
+                    self.__score += clue_cell.setCurrentState(CellStateEnum.MARKED)
                 self.__remainingClues -= 1
