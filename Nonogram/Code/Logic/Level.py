3f4e3fbff205e27924363df5edbee454effcb798
MVergar4 <marcvergara2023@udec.cl>
2024-11-25 23:51:18 -0300

    Implemented the gamemode selector in Level, current gameplay unchanged

--- Nonogram/Code/Logic/Level.py
+++ Nonogram/Code/Logic/Level.py
@@ -1,17 +1,19 @@
 from Grid import Grid
 from CellStateEnum import CellStateEnum
 from NoCluesRemainingException import NoCluesRemainingException
-
+import GameModeEnum
 
 class Level:
     __currentGrid : Grid
     __score : int
     __remainingClues : int
+    __gameMode : GameModeEnum
 
-    def __init__(self, expected : Grid) -> None:
+    def __init__(self, expected : Grid, mode : GameModeEnum) -> None:
         self.__currentGrid = expected
         self.__score = 0
         self.__remainingClues = 3
+        self.__gameMode = mode
         for i in range(self.__currentGrid.getGridRows()):
             for j in range(self.__currentGrid.getGridColumns()):
                 if self.__currentGrid.getCell(i, j).isSolved():
@@ -22,16 +24,23 @@ class Level:
         return self.__score
 
     def changeCell(self, i : int, j : int, state : CellStateEnum) -> None:
-        self.__score += self.__currentGrid.getCell(i, j).setCurrentState(state)
+        if self.__gameMode == GameModeEnum.ZEN:
+            self.__score += self.__currentGrid.getCell(i, j).setCurrentState(state)
 
     def useClue(self, i : int, j : int) -> None:
-        if self.__remainingClues > 0:
-            clue_cell = self.__currentGrid.getCell(i, j)
-            if not clue_cell.isSolved():
-                if clue_cell.getExpectedState() == CellStateEnum.PAINTED:
-                    self.__score += clue_cell.setCurrentState(CellStateEnum.PAINTED)
-                else:
-                    self.__score += clue_cell.setCurrentState(CellStateEnum.MARKED)
-                self.__remainingClues -= 1
-        else:
-            raise NoCluesRemainingException("No quedan pistas para usar")
+        if self.__gameMode == GameModeEnum.ZEN:
+            if self.__remainingClues > 0:
+                clue_cell = self.__currentGrid.getCell(i, j)
+                if not clue_cell.isSolved():
+                    if clue_cell.getExpectedState() == CellStateEnum.PAINTED:
+                        self.__score += clue_cell.setCurrentState(CellStateEnum.PAINTED)
+                    else:
+                        self.__score += clue_cell.setCurrentState(CellStateEnum.MARKED)
+                    self.__remainingClues -= 1
+            else:
+                raise NoCluesRemainingException("No quedan pistas para usar")
+        
+    def getGameMode(self) -> GameModeEnum:
+        return self.__gameMode
+    def setGameMode(self, change : GameModeEnum) -> None:
+        self.__gameMode = change
