a61f1c882de42f2113efad34f7551d751a375b9b
Xaidev <neclxas3311@gmail.com>
2024-12-02 09:32:30 -0300

    BotonS

--- Nonogram/Code/Visual/menu.py
+++ Nonogram/Code/Visual/menu.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from .boton import Boton
 from .panel import Panel
 
@@ -14,8 +15,10 @@ class Menu(Panel):
             Boton("Tutorial", (window_width//2 - 100, 450), (200, 55),
                   ((0, 0, 0), (255, 255, 255)), self.ir_a_tutorial),
             Boton("Opciones", (window_width//2 - 100, 550), (200, 55),
-                  ((0, 0, 0), (255, 255, 255)), self.ir_a_opciones)
+                  ((0, 0, 0), (255, 255, 255)), self.ir_a_opciones),
         ]
+        self.botonSalir = Boton("Salir", (0, 0), (200, 50),
+                                ((0, 0, 0), (255, 255, 255)), self.salir)
         pygame.font.init()
         self.fuente = pygame.font.Font(None, 100)
         self.fondo_imagen = pygame.image.load("Imagenes/Menu fondo.png")
@@ -38,6 +41,11 @@ class Menu(Panel):
     def manejar_evento(self, evento):
         for boton in self.botones:
             boton.manejar_evento(evento)
+        self.botonSalir.manejar_evento(evento)
+
+    def salir(self):
+        pygame.quit()
+        sys.exit()
 
     def dibujar(self, ventana):
         ventana.fill((80, 80, 80))
@@ -48,6 +56,6 @@ class Menu(Panel):
             boton.updatePosX((width//2 - 96, height//2 + 80 * count + 45))
             boton.dibujar(ventana)
             count += 1
-
+        self.botonSalir.dibujar(ventana)
         texto = self.fuente.render("Nonograma", True, (255, 255, 255))
         ventana.blit(texto, (width / 2 - 193, height/2-145))
